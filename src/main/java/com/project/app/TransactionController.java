3cccd4fde753c3a41f0372e917d9d3143d3bcdb3
Kevin Varga <varnorbi@gmail.com>
2021-04-26 15:07:25 -0500

    Adding final comments

--- src/main/java/com/project/app/TransactionController.java
+++ src/main/java/com/project/app/TransactionController.java
@@ -19,10 +19,12 @@ public class TransactionController {
     }
 
     @RequestMapping(path = "/points", method = RequestMethod.POST)
-    public Transaction person (@Valid @RequestBody Transaction points) {
+    public Transaction transaction (@Valid @RequestBody Transaction points) {
 
         List<Transaction> transactions;
+        //Check to see if payer already exists.
         if (allTransactions.containsKey(points.getPayer())) {
+            //If so get current transactions.
             transactions = allTransactions.get(points.getPayer());
         } else {
             transactions = new ArrayList<>();
@@ -36,7 +38,7 @@ public class TransactionController {
     @RequestMapping(path = "/balance", method = RequestMethod.GET)
     public Map<String, Integer> points () {
         Map<String, Integer> balance = new HashMap<>();
-
+        //Looping through the Map of all the transactions we have and summing together to get total for each payer.
         for (Map.Entry<String, List<Transaction>> pair : allTransactions.entrySet()) {
             balance.put(pair.getKey(), pair.getValue().stream().mapToInt(Transaction::getPoints).sum());
         }
@@ -46,13 +48,17 @@ public class TransactionController {
 
     @RequestMapping(path = "/spend", method = RequestMethod.POST)
     public Map<String, Integer> spend (@Valid @RequestBody Transaction points) {
+        //Getting just the points from the transaction cause that's all we care about.
         Integer pointsValue = points.getPoints();
         Map<String, Integer> spent = new HashMap<>();
+        //Creating a list of all transactions we have ordered by date.
         List<Transaction> orderedTransactions = allTransactions.values().stream()
                 .flatMap(List::stream)
                 .sorted(Comparator.comparingLong(point -> point.getTimestamp().getTime()))
                 .collect(Collectors.toList());
 
+        //Looping through the ordered transactions either till we have run out of transactions
+        //or we have used up all the points to spend.
         for (int i = 0; i < orderedTransactions.size() && pointsValue > 0; i++) {
             Transaction point = orderedTransactions.get(i);
             if (point.getPoints() < pointsValue) {
